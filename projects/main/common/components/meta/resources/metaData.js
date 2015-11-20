6364741ce9ae9083248d839e8503a1024c5bef6e
tamtakoe <tamtakoe@gmail.com>
2015-11-20 08:41:04 +0300

    some fixes

--- projects/main/common/components/meta/resources/metaData.js
+++ projects/main/common/components/meta/resources/metaData.js
@@ -5,7 +5,7 @@ define([
 ], function(app) {
     'use strict';
 
-    app.factory('MetaData', function(apiResource, metaDataResourceInterceptor, extendMeta) {
+    app.factory('MetaData', function(apiResource, metaDataResourceInterceptor, extendMeta, config) {
 
         var MetaData = apiResource('metadata/:id', {id: '@id'}, {
             get: {method: 'GET'},
@@ -18,30 +18,39 @@ define([
 
             metaData.$promise = MetaData.queryExtendedFormat().$promise
                 .then(function(data) {
-                    var metaDataItem = {//set default values from root URL
-                        $data:   angular.copy(data[0].$params.root.$data),
-                        $params: data[0].$params
-                    };
+                    return extendMetaData(data, params); //Why is not overwritten metaData.$promise?
+                })
+                .catch(function() {
+                    var data = metaDataResourceInterceptor.response((config.meta || []).concat([]));
 
-                    if (params.path) {//check current URL to match the patterns from the meta data collection
-                        for (var i = 0; i < data.length; i++) {
-                            //pattern `/:foo?bar` + path with params `/hello?bar=world` -> params `{foo: 'hello', bar: 'world'}` or null if no matches
-                            var urlParams = data[i].$urlMatcher.exec(params.path, params.search);
-
-                            if (urlParams) {
-                                angular.extend(metaDataItem.$params, data[i].$params);
-                                metaDataItem.$params.url = urlParams;
-
-                                extendMeta(metaDataItem.$data, data[i].$data);
-                            }
-                        }
-                    }
-
-                    return angular.extend(metaData, metaDataItem); //Why is not overwritten metaData.$promise?
+                    return extendMetaData(data, params); //Why is not overwritten metaData.$promise?
                 });
 
             return metaData;
         };
+
+        function extendMetaData(data, params) {
+            var metaDataItem = {//set default values from root URL
+                $data:   angular.copy(data[0].$params.root.$data),
+                $params: data[0].$params
+            };
+
+            if (params.path) {//check current URL to match the patterns from the meta data collection
+                for (var i = 0; i < data.length; i++) {
+                    //pattern `/:foo?bar` + path with params `/hello?bar=world` -> params `{foo: 'hello', bar: 'world'}` or null if no matches
+                    var urlParams = data[i].$urlMatcher.exec(params.path, params.search);
+
+                    if (urlParams) {
+                        angular.extend(metaDataItem.$params, data[i].$params);
+                        metaDataItem.$params.url = urlParams;
+
+                        extendMeta(metaDataItem.$data, data[i].$data);
+                    }
+                }
+            }
+
+            return angular.extend(MetaData, metaDataItem);
+        }
 
         return MetaData;
     });
